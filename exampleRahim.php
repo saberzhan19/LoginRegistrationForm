c223ac8790ded95f5edba71c1fefc4077eb65540
Saberzhan19 <Saberzhax@gmail.com>
2021-05-04 06:37:59 +0600

    Lessons

--- exampleRahim.php
+++ exampleRahim.php
@@ -6,9 +6,10 @@ class Animal
     //эти свойства, теже самые переменные
     public $name;
     public $age;
+    const ID = 10;//это константа, к/й значение не меняеться 
 
     //конструктор класса
-    public function __construct($name, $age){
+    public function __construct($name, Int $age){//Int, String, bool эти типы данных, при к/х выводимые данные не меняються. Например если цифры, то только цифры выводяться
         $this->name= $name;
         $this->age= $age;
     }
@@ -29,9 +30,12 @@ class Animal
     public function sayAge(){
         return $this->age;
     }
+    public static function saySomething(){//статичный метод от слова 'static' 
+        echo 'She wow - wow';
+    }
 }
 
-//Внизу всегда отдельно, сверху классы, снизу обьекты
+/* //Внизу всегда отдельно, сверху классы, снизу обьекты
 $myAnimal = new Animal('Sapura','28<br>');
 $myAnimal2 = new Animal('Dina',27);
 
@@ -43,3 +47,40 @@ echo $myAnimal->age;
 echo $myAnimal2->name;
 echo $myAnimal2->age;
 
+//Вот так обращаються Константе
+echo $myAnimal::ID;
+*/
+// Animal::saySomething();//статичный метод проще вызвать
+
+// $myAnimal = new Animal('Samal', 'asds');//здесь примеры от типов Int, String, bool 
+// echo $myAnimal->age;
+
+/* 
+class Univercity{
+    public $students;
+
+    public function __construct(array $students){
+        var_dump($students);
+    }
+}
+new Univercity(['dady', '123']);//(students:123)так выдает ошибку, но если прописать [''], то нет ошибок */
+
+class Student{
+    public $name = 'She is a student';
+    public $age = 29;
+}
+
+class Univercity{
+    public $students;
+
+    public function addStudent(Student $student){//Здесь меняем Student
+        echo $student->age;
+    }
+}
+
+$univer = new Univercity;
+
+$animal = new Animal('Panda', 15);
+$student = new Student();
+
+$univer->addStudent($student);//Здесь меняем $student и от зависимости class Student или Univercity
